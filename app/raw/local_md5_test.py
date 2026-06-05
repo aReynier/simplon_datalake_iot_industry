724529dc2f9f266f6587dd0d993ddf0c16511333
Aurore Reynier <reynier.aurore@gmail.com>
2026-06-05 16:49:39 +0200

    feat: perform md5 operation on a csv file

---
+++ app/raw/local_md5_test.py
@@ -0,0 +1,15 @@
+import hashlib
+
+# def calculate_local_md5(file_name):
+    # hash_md5 = hashlib.md5()
+def calculate_local_md5():
+
+    with open(f"data_input/LineA_Stable_10K.csv", 'rb') as f:
+        file_content = f.read()
+        local_md5 = hashlib.md5(file_content).hexdigest()
+
+    print(local_md5)
+
+
+if __name__ == "__main__":
+    calculate_local_md5()
