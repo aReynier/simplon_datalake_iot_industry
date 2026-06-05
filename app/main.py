a81a710f45d9d573564a0f82f671a99a264aaca3
Aurore Reynier <reynier.aurore@gmail.com>
2026-06-05 16:30:00 +0200

    refactor: rearrange app folder and refactor script
    now data injection script work from main script

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-print("python well initiated")
+from raw.data_ingestion import ingest_data
+
+
+def main():
+    ingest_data("LineA_Stable_10K", "A")
+    ingest_data("LineB_Flux", "B")
+    ingest_data("LineC_Turbulent", "C")
+    ingest_data("LineD_SpikeControl", "D")
+    ingest_data("LineE_SmoothRun", "E")
+
+
+if __name__ == "__main__":
+    main()
