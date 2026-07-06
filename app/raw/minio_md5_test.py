7f752817ce01c4fb8c8400e49ce0f531979d884e
Aurore Reynier <reynier.aurore@gmail.com>
2026-07-06 08:15:45 +0200

    feat(raw): perform md5 operation on a minio bucket file

---
+++ app/raw/minio_md5_test.py
@@ -0,0 +1,25 @@
+import hashlib
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def calculate_minio_md5():
+    s3 = boto3.client('s3',
+            endpoint_url='http://localhost:9000',
+            aws_access_key_id=os.getenv("MINIO_ROOT_USER"),
+            aws_secret_access_key=os.getenv("MINIO_ROOT_PASSWORD"))
+    
+    object = s3.get_object(Bucket='raw', Key='production_lines/lineA/LineA_Stable_10K.csv')
+
+    file_bytes = object['Body'].read()
+
+    minio_md5 = hashlib.md5(file_bytes).hexdigest()
+
+
+    print(minio_md5)
+
+
+if __name__ == "__main__":
+    calculate_minio_md5()
