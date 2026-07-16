2b4d591863a508d65d1846cb5b7416e3d252f348
Aurore Reynier <reynier.aurore@gmail.com>
2026-07-16 22:50:02 +0200

    wip: staging dag airflow in progress

---
+++ app/staging/data_transformation.py
@@ -0,0 +1,9 @@
+# Take from minio raw
+# harmonize column names
+# normalize timestamp format
+# deposit on staging
+def transform_data():
+    pass
+
+if __name__ == "__main__":
+    transform_data()
